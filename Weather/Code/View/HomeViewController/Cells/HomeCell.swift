2686ea1bff6d60055e8fc6456662f64e2bc7e5ae
Habib Matthieu <habib.matthieu1@gmail.com>
2020-01-19 22:18:00 +0100

    Ajout des icônes dans les cell pour les temps : pluvieux, orageux, nuageux, venteux, et neigeux

--- Weather/Code/View/HomeViewController/Cells/HomeCell.swift
+++ Weather/Code/View/HomeViewController/Cells/HomeCell.swift
@@ -23,10 +23,41 @@ class HomeCell: UICollectionViewCell {
     //MARK: Configure Cell
     func configurewith(_ weather: WeatherProtocol, at index: Int) {
         self.backgroundColor = WeatherUtils.weatherColor(index: index)
-        self.dateLabel.text = WeatherUtils.changeDateFormat(dateString: weather.date) 
+        self.dateLabel.text = WeatherUtils.changeDateFormat(dateString: weather.date)
+//        print(dateLabel.text)
         self.temperatureLabel.text = String(format: "%.0f", weather.temperature! - 273.15) + "°C"
         if #available(iOS 13.0, *) {
-            if(weather.rain! > 50) { weatherImageView.image = UIImage(systemName: "cloud.rain") }
+            if(weather.temperature! > 293.15) { //si jour + hot
+                self.weatherImageView.image = UIImage(systemName: "thermometer.sun")
+                
+                if(weather.wind! > 25.0) { self.weatherImageView.image = UIImage(systemName: "wind")} //vent
+                if(weather.rain! > 15.0) { self.weatherImageView.image = UIImage(systemName:  "cloud.rain")} //pluie
+            }
+            if(weather.temperature! < 293.15 && weather.temperature! > 283.15) { //si jour + soleil
+                self.weatherImageView.image = UIImage(systemName: "sun.max")
+                
+                if(weather.wind! > 25.0) { self.weatherImageView.image = UIImage(systemName: "wind")} //vent
+                if(weather.rain! > 15.0) { self.weatherImageView.image = UIImage(systemName:  "cloud.rain")} //pluie
+            }
+            if(weather.temperature! < 283.15 && weather.temperature! > 273.15) { //si jour + nuageux
+                self.weatherImageView.image = UIImage(systemName: "moon")
+                
+                if(weather.wind! > 25.0) { self.weatherImageView.image = UIImage(systemName: "wind")} //vent
+                if(weather.rain! > 15.0) { self.weatherImageView.image = UIImage(systemName:  "cloud.rain")} //pluie
+            }
+            if(weather.temperature! < 273.15 && weather.temperature! > 263.15) { //si jour + nuageux
+                self.weatherImageView.image = UIImage(systemName: "thermometer.snowflake")
+                
+                if(weather.wind! > 25.0) { self.weatherImageView.image = UIImage(systemName: "wind")} //vent
+                if(weather.rain! > 15.0) { self.weatherImageView.image = UIImage(systemName:  "cloud.snow")} //neige
+            }
+            if(weather.temperature! < 263.15) { //si jour + glaciale
+                self.weatherImageView.image = UIImage(systemName: "thermometer.snowflake")
+                
+                if(weather.wind! > 25.0) { self.weatherImageView.image = UIImage(systemName: "wind")} //vent
+                if(weather.rain! > 15.0) { self.weatherImageView.image = UIImage(systemName:  "cloud.snow")} //neige
+            }
+            
         }
     }
 }
